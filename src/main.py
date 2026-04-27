85ae85d39a172945d52b00dd8df6413b364765ff
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-04-27 17:11:34 -0300

    feat(transform): add schemas unificado
    see #26

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
-def hello():
-    return 'Hello, world!'
+from frictionless import Package, Schema
+
+general_schemas = Schema('datapackages/schemas.yaml')
+names = [field.name for field in general_schemas.fields]
+
+package = Package("datapackages/dados_siafi/raw_datapackage.yaml")
+
+for resource in package.resources:
+    schema = resource.schema.fields
+    for index, field in enumerate(schema):
+        if field.custom['target'] in names:
+            new_field = [new_field for new_field in general_schemas.fields if new_field.name == field.custom['target']][0]
+            schema[index] = new_field
+
+package
+package.to_json('datapackages/dados_siafi/datapackage.json')
 
 
-if __name__ == '__main__':
-    print(hello())
