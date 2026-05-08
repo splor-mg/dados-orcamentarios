b90d3c706790f77cbbfcd168cabb630c1a22f17a
Maria Duque <maria.oliveira.duque@gmail.com>
2026-05-08 14:57:37 -0300

    cria função para alterar datapackage após extract

---
+++ src/datapackage_raw.py
@@ -0,0 +1,28 @@
+from frictionless import Package, Schema
+from pathlib import Path
+
+
+def apply_common_fields():
+    common_schema = Schema('datapackages/common.yaml')
+    names = [field.name for field in common_schema.fields]
+
+    datapackages = Path('datapackages').glob('*/datapackage.yaml')
+
+    for datapackage in datapackages:
+        package = Package(datapackage)
+
+        for resource in package.resources:
+            schema = resource.schema.fields
+
+            for index, field in enumerate(schema):
+                target = field.custom['target']
+
+                if target in names:
+                    common_field = [f for f in common_schema.fields if f.name == target][0]
+
+                    schema[index] = common_field.to_copy(name=field.name)
+                    schema[index].custom['target'] = target
+
+            resource.custom.pop('dpetl_extract', None)
+
+        package.to_yaml(datapackage.parent / 'datapackage.yaml')
