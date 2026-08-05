bf9fafe6af65bb017f919881c9cc076d9a409e14
Maria Duque <maria.oliveira.duque@gmail.com>
2026-08-05 09:21:06 -0300

    cria ano dinamicamente

--- src/build_datapackage.py
+++ src/build_datapackage.py
@@ -1,23 +1,28 @@
+import yaml
+from datetime import datetime
 from frictionless import Package, Schema
 from pathlib import Path
 
 
+year = str(datetime.now().year)
+
 datapackages = Path('datapackages').glob('*/raw_datapackage.yaml')
 common_schema = Schema('datapackages/common.yaml')
-names = [field.name for field in common_schema.fields]
+common_fields = {field.name: field for field in common_schema.fields}
 
 for datapackage in datapackages:
-    package = Package(datapackage)
+    text = datapackage.read_text(encoding='utf-8').replace('{{year}}', year)
+    descriptor = yaml.safe_load(text)
+    package = Package(descriptor, basepath=str(datapackage.parent))
 
     for resource in package.resources:
         schema = resource.schema.fields
 
         for index, field in enumerate(schema):
-            target = field.custom['target']
+            target = field.custom.get('target')
 
-            if target in names:
-                common_field = [f for f in common_schema.fields if f.name == target][0]
-
+            if target and target in common_fields:
+                common_field = common_fields[target]
                 schema[index] = common_field.to_copy(name=field.name)
                 schema[index].custom['target'] = target
 
